fbf6f42a6b0f2d369b120c587a97f11e24ed6f6c
Armando Uch <armando.uch@pm.me>
2024-11-05 12:36:31 -0600

    Add script instalations

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
 echo "Instalation.."
+
+rm -rfv ~/.config/my-zsh-configs
+
+git clone git@github.com:jahrmando/my-zsh-configs.git ~/.config/my-zsh-configs 
+
+if [[ -n ${MYZSH_DEBUG}  ]]; then
+  cd ~/.config/my-zsh-configs &&  git checkout initial-configs 
+fi
+
+cp ~/.zshrc ~/.zshrc.backup && cp -fv ~/.config/my-zsh-configs/.zshrc_custom ~/.zshrc 
+
+echo "Install plugins"
+
+cd ~/.config/my-zsh-configs/plugins/zsh-autosuggestions/ && git submodule init && git submodule update
+cd ~/.config/my-zsh-configs/plugins/zsh-syntax-highlighting/ && git submodule init && git submodule update
+
+echo "Reload your ZSH: omz reload"
+
+which brew
+if [[ $? == "0" ]]; then
+  echo "Instalation tools from brew package manager"
+  brew install git nvim grep ccat
+fi
--- README.md
+++ README.md
@@ -1,11 +1,25 @@
 # my-zsh-configs
 
-My custom configs to use oh-my-zsh
+My custom configs to use oh-my-zsh. Only works on MacOS
+
+# Pre Installation 
+
+## oh-my-zsh
+
+```bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+```
+
+## Homebrew 
+
+```bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+```
 
 # Installation 
 
 Please run this on your CLI if you trust me ;-)
 
-```
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/jahrmando/my-zsh-configs/initial-configs/INSTALL.sh)"
+```bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/jahrmando/my-zsh-configs/main/INSTALL.sh)"
 ```
