ddcbb3b8a6d870ebbfcb47e4245c8a5b9de3e330
Arm.do <armando.uch@pm.me>
2024-11-05 12:39:10 -0600

    Initial configs (#1)
    * Add new initial configs
    
    * Update readme
    
    * Add a custom zsh config
    
    * Add base plugins
    
    * Add script instalations

---
+++ aliases.zsh
@@ -0,0 +1,13 @@
+alias vim="nvim"
+alias _vim="/usr/bin/vim"
+# alias cat="bat"
+# alias ccat="/bin/cat"
+#
+## External 
+alias yolo='git commit -m "$(curl -s http://whatthecommit.com/index.txt)"'
+alias wtf_ip='echo "$(curl -s https://wtfismyip.com/json)"'
+#
+## Macos Only
+alias hide_files_disable="defaults write com.apple.finder AppleShowAllFiles TRUE;killall Finder"
+alias hile_files_enable="defaults write com.apple.finder AppleShowAllFiles FALSE;killall Finder"
+
